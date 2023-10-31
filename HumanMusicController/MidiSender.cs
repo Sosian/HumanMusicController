d3954626dfec1f439633cd7d9500a27cf9749232
Florian Decker <florian@deckerhome.at>
2023-10-31 19:31:20 +0100

    Fix times not working out, some small changes

--- HumanMusicController/MidiSender.cs
+++ HumanMusicController/MidiSender.cs
@@ -7,30 +7,38 @@ namespace HumanMusicController
 {
     public class MidiSender : IDisposable
     {
-        private readonly string midiPortName;
         private readonly OutputDevice outputDevice;
+
+        private List<int> listOfHeartrates = new List<int>();
+
         private int lastPlayedNote = 0;
 
         public MidiSender(string midiPortName)
         {
-            this.midiPortName = midiPortName;
             outputDevice = OutputDevice.GetByName(midiPortName);
         }
 
         public void SendMidiMsg(int heartrate)
         {
+            listOfHeartrates.Add(heartrate);
+
             var key = heartrate - 40;
             if (lastPlayedNote == 0)
             {
                 outputDevice.SendEvent(new NoteOnEvent(new SevenBitNumber((byte)key), new SevenBitNumber((byte)key)));
                 lastPlayedNote = key;
             }
-            else
+            else if (LastXItemsAreNotTheSame(listOfHeartrates, 5))
             {
                 outputDevice.SendEvent(new NoteOffEvent(new SevenBitNumber((byte)lastPlayedNote), new SevenBitNumber((byte)lastPlayedNote)));
                 lastPlayedNote = key;
                 outputDevice.SendEvent(new NoteOnEvent(new SevenBitNumber((byte)key), new SevenBitNumber((byte)key)));
             }
+        }
+
+        private bool LastXItemsAreNotTheSame(IEnumerable<int> listOfItems, int numberOfItems)
+        {
+            return listOfItems.Reverse().Take(numberOfItems).Distinct().Count() != 1;
         }
 
         public void Dispose()
